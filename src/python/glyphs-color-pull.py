94e9003706ebf1126a76ac6a91c361cea529585c
Chris Simpkins <git.simpkins@gmail.com>
2019-02-04 11:23:06 -0500

    another update to copyright...

--- src/python/glyphs-color-pull.py
+++ src/python/glyphs-color-pull.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright 2019 Font Bakery Authors and Contributors
+# Copyright 2019 Font Bakery Reporter Authors and Contributors
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
